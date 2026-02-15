c0991279240915f83815770285d6bbf0d7b19cab
Dwima Basumatary <dwima1583@gmail.com>
2026-02-15 20:06:32 +0530

    nodejs dns issue resolve

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@ import app from "./app.js"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
+import { setServers } from "node:dns/promises";
+setServers(["1.1.1.1", "8.8.8.8"]);
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI!)
   .then(() => console.log("DB Connected"))
   .catch(err => console.error(err))
 
+  
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
 })
