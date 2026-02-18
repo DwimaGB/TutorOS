402c83e7e7b24c7cd97c92bb6c7c89b6c505033c
Dwima Basumatary <124419527+DwimaGB@users.noreply.github.com>
2026-02-18 22:08:30 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,8 @@ import app from "./app.js"
 import { connectDB } from "./config/db.js"
 import { cloudinary } from "./config/cloudinary.js"
 
-import { setServers } from "node:dns/promises";
-setServers(["1.1.1.1", "8.8.8.8"]); // Set DNS servers to Cloudflare
+// import { setServers } from "node:dns/promises";
+// setServers(["1.1.1.1", "8.8.8.8"]); // Set DNS servers to Cloudflare
 
 const PORT = process.env.PORT || 5000
 
